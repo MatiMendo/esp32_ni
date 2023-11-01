50586a0d6f83ddcd184259734ae5ce797d5deb35
Matias Mendoza <juanmati3mendoza@gmail.com>
2023-11-01 11:24:08 -0300

    cambio

--- main.py
+++ main.py
@@ -16,26 +16,28 @@ led = Pin(2, Pin.OUT)
 d = dht.DHT11(Pin(25))
 contador = 0
 bandera=False
+liminf=22
+limsup=29
 
-#def heuartbeat(nada):
-    #global contador
-    #if contador > 5:
-      #  pulsos.deinit()
-       # contador = 0
-        #return
-    #led.value(not led.value())
-    #contador += 1
+def heuartbeat(nada):
+    global contador
+    if contador > 5:
+        pulsos.deinit()
+        contador = 0
+        return
+    led.value(not led.value())
+    contador += 1
   
 def transmitir(pin):
     print("publicando  " + CLIENT_ID)
     mqtt.connect()
     mqtt.publish(f"testtopic/{CLIENT_ID}",datos)
     mqtt.disconnect()
-    #pulsos.init(period=150, mode=Timer.PERIODIC, callback=heartbeat)
+    pulsos.init(period=150, mode=Timer.PERIODIC, callback=heartbeat)
 
 #publicar = Timer(0)
 #publicar.init(period=30000, mode=Timer.PERIODIC, callback=transmitir)
-#pulsos = Timer(1)
+pulsos = Timer(1)
 
 while True:
     try:
@@ -47,9 +49,15 @@ while True:
             ('humedad',humedad)
         ]))
         print(datos)
-        if temperatura>=29 and bandera==False: 
-            transmitir(None)
+
+        if temperatura<liminf and bandera==False:
             bandera=True
+            print ("La temperatura esta por debajo del limite inferior")
+
+        if temperatura>limsup and bandera==True: 
+            transmitir(None)
+            bandera=False
+
     except OSError as e:
         print("sin sensor")
     time.sleep(5)
