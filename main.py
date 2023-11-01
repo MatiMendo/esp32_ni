5266d62aace46b8c492e5c5dc0d95e2170d1cc47
Matias Mendoza <juanmati3mendoza@gmail.com>
2023-11-01 01:16:17 -0300

    cambios

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Germán Andrés Xander 2023
-
 from machine import Pin, Timer, unique_id
 import dht
 import time
@@ -17,26 +15,27 @@ mqtt = MQTTClient(CLIENT_ID, SERVIDOR_MQTT,
 led = Pin(2, Pin.OUT)
 d = dht.DHT11(Pin(25))
 contador = 0
+bandera=False
 
-def heartbeat(nada):
-    global contador
-    if contador > 5:
-        pulsos.deinit()
-        contador = 0
-        return
-    led.value(not led.value())
-    contador += 1
+#def heuartbeat(nada):
+    #global contador
+    #if contador > 5:
+      #  pulsos.deinit()
+       # contador = 0
+        #return
+    #led.value(not led.value())
+    #contador += 1
   
 def transmitir(pin):
-    print("publicando "+CLIENT_ID)
+    print("publicando  " + CLIENT_ID)
     mqtt.connect()
     mqtt.publish(f"testtopic/{CLIENT_ID}",datos)
     mqtt.disconnect()
-    pulsos.init(period=150, mode=Timer.PERIODIC, callback=heartbeat)
+    #pulsos.init(period=150, mode=Timer.PERIODIC, callback=heartbeat)
 
-publicar = Timer(0)
-publicar.init(period=30000, mode=Timer.PERIODIC, callback=transmitir)
-pulsos = Timer(1)
+#publicar = Timer(0)
+#publicar.init(period=30000, mode=Timer.PERIODIC, callback=transmitir)
+#pulsos = Timer(1)
 
 while True:
     try:
@@ -48,6 +47,10 @@ while True:
             ('humedad',humedad)
         ]))
         print(datos)
+        if temperatura>=29 and bandera==False: 
+            transmitir(None)
+            bandera=True
     except OSError as e:
         print("sin sensor")
     time.sleep(5)
+
